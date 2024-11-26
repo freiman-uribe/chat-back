233b77b29ef017d19a82d44a4ea172015d16bf02
FreimanRuiz <freiman@bewe.io>
2024-11-26 14:08:04 -0500

    finish back

--- model/modelChat.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose')
-
-// const ChatSchema = new mongoose.Schema({
-
-// })
-
-// const Chat = mongoose.model('chat', ChatSchema)
-
-// module.exports = Chat
--- server.js
+++ server.js
@@ -1,21 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-require('dotenv').config();
-
-const connectDB = require('./config/connectMongo');
-connectDB();
-
-const chatRoutes = require('./routes/chat');
-
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const cors = require("cors");
+const connectDB = require("./config/connectMongo");
+const authRoutes = require("./routes/auth");
+const chatRoutes = require("./routes/chat");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
+  },
+});
+require("dotenv").config();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+connectDB();
 
-app.use('/chat', chatRoutes)
+// Habilitar CORS para permitir cualquier origen y cualquier tipo de petición
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
+  })
+);
 
-const PORT = process.env.PORT || 3000
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes(io)); // Pasar io a las rutas
 
-app.listen(PORT, ()=> console.log(`Runing in port http://localhost:${PORT}`))
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.on("disconnect", () => {
+    console.log("User has left");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
